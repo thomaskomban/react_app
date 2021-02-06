411791da2f7ea6e6badb8b605f7760c37e039d1b
admin <admin@192-168-1-101.tpgi.com.au>
2021-02-06 13:48:39 +1100

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
+// eslint-disable-next-line
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// setup vars
+const books = [{
+  id: 1,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
+  title: 'I Love You to the Moon and -Back',
+  author: 'Amelia Hepworth',
+},
+{
+  id:2,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
+  title: 'I Love You to the Moon and -Back',
+  author: 'Amelia Hepworth',
+},
+{
+  id:3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
+  title: 'I Love You to the Moon and -Back',
+  author: 'Amelia Hepworth',
+}];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    // <section className='booklist'>
+    //   {Books}      
+    // </section>
+    <section className='booklist'>
+      {books.map((book, index) => {
+        return <Book key={index} {...book}></Book>
+      })}      
+    </section>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const Books= (books.map((book, index) => {
+//     return (
+//     <article key={index} className='book'>
+//       <img src={book.img} alt=""/>
+//       <h1>{book.title}</h1>
+//       <h4>{book.author}</h4>      
+//     </article>
+//   );
+// }))
+const complexExample = (author) => {
+  console.log(author);
+}
+const Book = (props) => {  
+  const clickHandler = (t) => {
+    alert(t);
+  }
+  return (
+    <article className='book' onMouseOver={()=>{
+      console.log(props.title);
+    }}>
+      <img src={props.img} alt=""/>
+      <h1 onClick={() => clickHandler(props.title)}>{props.title}</h1>
+      <h4>{props.author}</h4>   
+      <button type="button" onClick={() => clickHandler(props.id)}>reference example</button>  
+      <button type="button" onClick={() => complexExample(props.author)}>more complex example</button> 
+    </article>
+  )
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
